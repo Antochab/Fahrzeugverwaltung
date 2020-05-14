c04c21ff80336f01a8cd2659bde7be5e5bf60204
Anton Boger <Anton.Boger@edu.fhdw.de>
2020-05-14 17:04:04 +0200

    Projektdateien hinzufügen.

---
+++ Fahrzeugverwaltung/LKW.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeugverwaltung
+{
+    class LKW : Fahrzeug
+    {
+        private int achsenAnzahl;
+        private int zuladung;
+    }
+}
