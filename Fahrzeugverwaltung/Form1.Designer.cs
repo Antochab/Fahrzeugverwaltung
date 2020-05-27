ff9f1a804cda32b8dab54727d78f7d54b0afe677
Anton Boger <Anton.Boger@edu.fhdw.de>
2020-05-27 15:53:31 +0200

    Benutzeroberfläche für das Anlegen von Autos, ohne Anbindung an die BusinessLogik

--- Fahrzeugverwaltung/Form1.Designer.cs
+++ Fahrzeugverwaltung/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fahrzeugverwaltung
 {
-    partial class Form1
+    partial class FormAutoAnlegen
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxHersteller = new System.Windows.Forms.TextBox();
+            this.textBoxModell = new System.Windows.Forms.TextBox();
+            this.textBoxKennzeichen = new System.Windows.Forms.TextBox();
+            this.textBoxZulassung = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxAnschaffungspreis = new System.Windows.Forms.TextBox();
+            this.textBoxHubraum = new System.Windows.Forms.TextBox();
+            this.textBoxLeistung = new System.Windows.Forms.TextBox();
+            this.textBoxSchadstoffklasse = new System.Windows.Forms.TextBox();
+            this.labelHersteller = new System.Windows.Forms.Label();
+            this.labelModell = new System.Windows.Forms.Label();
+            this.labelKennzeichen = new System.Windows.Forms.Label();
+            this.labelZulassung = new System.Windows.Forms.Label();
+            this.labelAnschaffungspreis = new System.Windows.Forms.Label();
+            this.labelHubraum = new System.Windows.Forms.Label();
+            this.labelLeistung = new System.Windows.Forms.Label();
+            this.labelSchadstoffklasse = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxHersteller
+            // 
+            this.textBoxHersteller.Location = new System.Drawing.Point(206, 15);
+            this.textBoxHersteller.Name = "textBoxHersteller";
+            this.textBoxHersteller.Size = new System.Drawing.Size(100, 26);
+            this.textBoxHersteller.TabIndex = 0;
+            // 
+            // textBoxModell
+            // 
+            this.textBoxModell.Location = new System.Drawing.Point(206, 56);
+            this.textBoxModell.Name = "textBoxModell";
+            this.textBoxModell.Size = new System.Drawing.Size(100, 26);
+            this.textBoxModell.TabIndex = 1;
+            // 
+            // textBoxKennzeichen
+            // 
+            this.textBoxKennzeichen.Location = new System.Drawing.Point(206, 107);
+            this.textBoxKennzeichen.Name = "textBoxKennzeichen";
+            this.textBoxKennzeichen.Size = new System.Drawing.Size(100, 26);
+            this.textBoxKennzeichen.TabIndex = 2;
+            // 
+            // textBoxZulassung
+            // 
+            this.textBoxZulassung.Location = new System.Drawing.Point(206, 159);
+            this.textBoxZulassung.Name = "textBoxZulassung";
+            this.textBoxZulassung.Size = new System.Drawing.Size(100, 26);
+            this.textBoxZulassung.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelSchadstoffklasse);
+            this.panel1.Controls.Add(this.labelLeistung);
+            this.panel1.Controls.Add(this.labelHubraum);
+            this.panel1.Controls.Add(this.labelAnschaffungspreis);
+            this.panel1.Controls.Add(this.labelZulassung);
+            this.panel1.Controls.Add(this.labelKennzeichen);
+            this.panel1.Controls.Add(this.labelModell);
+            this.panel1.Controls.Add(this.labelHersteller);
+            this.panel1.Controls.Add(this.textBoxSchadstoffklasse);
+            this.panel1.Controls.Add(this.textBoxLeistung);
+            this.panel1.Controls.Add(this.textBoxHubraum);
+            this.panel1.Controls.Add(this.textBoxAnschaffungspreis);
+            this.panel1.Controls.Add(this.textBoxHersteller);
+            this.panel1.Controls.Add(this.textBoxZulassung);
+            this.panel1.Controls.Add(this.textBoxModell);
+            this.panel1.Controls.Add(this.textBoxKennzeichen);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(642, 396);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBoxAnschaffungspreis
+            // 
+            this.textBoxAnschaffungspreis.Location = new System.Drawing.Point(206, 215);
+            this.textBoxAnschaffungspreis.Name = "textBoxAnschaffungspreis";
+            this.textBoxAnschaffungspreis.Size = new System.Drawing.Size(100, 26);
+            this.textBoxAnschaffungspreis.TabIndex = 4;
+            // 
+            // textBoxHubraum
+            // 
+            this.textBoxHubraum.Location = new System.Drawing.Point(206, 272);
+            this.textBoxHubraum.Name = "textBoxHubraum";
+            this.textBoxHubraum.Size = new System.Drawing.Size(100, 26);
+            this.textBoxHubraum.TabIndex = 5;
+            // 
+            // textBoxLeistung
+            // 
+            this.textBoxLeistung.Location = new System.Drawing.Point(206, 321);
+            this.textBoxLeistung.Name = "textBoxLeistung";
+            this.textBoxLeistung.Size = new System.Drawing.Size(100, 26);
+            this.textBoxLeistung.TabIndex = 6;
+            // 
+            // textBoxSchadstoffklasse
+            // 
+            this.textBoxSchadstoffklasse.Location = new System.Drawing.Point(206, 369);
+            this.textBoxSchadstoffklasse.Name = "textBoxSchadstoffklasse";
+            this.textBoxSchadstoffklasse.Size = new System.Drawing.Size(100, 26);
+            this.textBoxSchadstoffklasse.TabIndex = 7;
+            // 
+            // labelHersteller
+            // 
+            this.labelHersteller.AutoSize = true;
+            this.labelHersteller.Location = new System.Drawing.Point(19, 18);
+            this.labelHersteller.Name = "labelHersteller";
+            this.labelHersteller.Size = new System.Drawing.Size(77, 20);
+            this.labelHersteller.TabIndex = 8;
+            this.labelHersteller.Text = "Hersteller";
+            // 
+            // labelModell
+            // 
+            this.labelModell.AutoSize = true;
+            this.labelModell.Location = new System.Drawing.Point(19, 60);
+            this.labelModell.Name = "labelModell";
+            this.labelModell.Size = new System.Drawing.Size(55, 20);
+            this.labelModell.TabIndex = 9;
+            this.labelModell.Text = "Modell";
+            // 
+            // labelKennzeichen
+            // 
+            this.labelKennzeichen.AutoSize = true;
+            this.labelKennzeichen.Location = new System.Drawing.Point(19, 111);
+            this.labelKennzeichen.Name = "labelKennzeichen";
+            this.labelKennzeichen.Size = new System.Drawing.Size(101, 20);
+            this.labelKennzeichen.TabIndex = 10;
+            this.labelKennzeichen.Text = "Kennzeichen";
+            // 
+            // labelZulassung
+            // 
+            this.labelZulassung.AutoSize = true;
+            this.labelZulassung.Location = new System.Drawing.Point(19, 163);
+            this.labelZulassung.Name = "labelZulassung";
+            this.labelZulassung.Size = new System.Drawing.Size(83, 20);
+            this.labelZulassung.TabIndex = 11;
+            this.labelZulassung.Text = "Zulassung";
+            // 
+            // labelAnschaffungspreis
+            // 
+            this.labelAnschaffungspreis.AutoSize = true;
+            this.labelAnschaffungspreis.Location = new System.Drawing.Point(19, 219);
+            this.labelAnschaffungspreis.Name = "labelAnschaffungspreis";
+            this.labelAnschaffungspreis.Size = new System.Drawing.Size(142, 20);
+            this.labelAnschaffungspreis.TabIndex = 12;
+            this.labelAnschaffungspreis.Text = "Anschaffungspreis";
+            // 
+            // labelHubraum
+            // 
+            this.labelHubraum.AutoSize = true;
+            this.labelHubraum.Location = new System.Drawing.Point(19, 276);
+            this.labelHubraum.Name = "labelHubraum";
+            this.labelHubraum.Size = new System.Drawing.Size(75, 20);
+            this.labelHubraum.TabIndex = 13;
+            this.labelHubraum.Text = "Hubraum";
+            // 
+            // labelLeistung
+            // 
+            this.labelLeistung.AutoSize = true;
+            this.labelLeistung.Location = new System.Drawing.Point(19, 325);
+            this.labelLeistung.Name = "labelLeistung";
+            this.labelLeistung.Size = new System.Drawing.Size(70, 20);
+            this.labelLeistung.TabIndex = 14;
+            this.labelLeistung.Text = "Leistung";
+            // 
+            // labelSchadstoffklasse
+            // 
+            this.labelSchadstoffklasse.AutoSize = true;
+            this.labelSchadstoffklasse.Location = new System.Drawing.Point(19, 373);
+            this.labelSchadstoffklasse.Name = "labelSchadstoffklasse";
+            this.labelSchadstoffklasse.Size = new System.Drawing.Size(132, 20);
+            this.labelSchadstoffklasse.TabIndex = 15;
+            this.labelSchadstoffklasse.Text = "Schadstoffklasse";
+            // 
+            // FormAutoAnlegen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.panel1);
+            this.Name = "FormAutoAnlegen";
+            this.Text = "Neues Auto anlegen";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxHersteller;
+        private System.Windows.Forms.TextBox textBoxModell;
+        private System.Windows.Forms.TextBox textBoxKennzeichen;
+        private System.Windows.Forms.TextBox textBoxZulassung;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxSchadstoffklasse;
+        private System.Windows.Forms.TextBox textBoxLeistung;
+        private System.Windows.Forms.TextBox textBoxHubraum;
+        private System.Windows.Forms.TextBox textBoxAnschaffungspreis;
+        private System.Windows.Forms.Label labelSchadstoffklasse;
+        private System.Windows.Forms.Label labelLeistung;
+        private System.Windows.Forms.Label labelHubraum;
+        private System.Windows.Forms.Label labelAnschaffungspreis;
+        private System.Windows.Forms.Label labelZulassung;
+        private System.Windows.Forms.Label labelKennzeichen;
+        private System.Windows.Forms.Label labelModell;
+        private System.Windows.Forms.Label labelHersteller;
     }
 }
 
--- Fahrzeugverwaltung/Form1.cs
+++ Fahrzeugverwaltung/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Fahrzeugverwaltung
 {
-    public partial class Form1 : Form
+    public partial class FormAutoAnlegen : Form
     {
-        public Form1()
+        public FormAutoAnlegen()
         {
             InitializeComponent();
         }
