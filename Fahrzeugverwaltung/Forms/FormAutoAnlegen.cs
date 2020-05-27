19f2e11a1a6c44239d586114929047c2de9e997e
Anton Boger <Anton.Boger@edu.fhdw.de>
2020-05-28 01:43:42 +0200

    Anbindung der Business Logik an die GUI zum Anlegen eines Fahrzeuges

---
+++ Fahrzeugverwaltung/Forms/FormAutoAnlegen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fahrzeugverwaltung
+{
+    public partial class FormAutoAnlegen : Form
+    {
+        Fahrzeugpool fahrzeugpool = new Fahrzeugpool();
+        public FormAutoAnlegen()
+        {
+            InitializeComponent();
+        }
+
+        //Hinzufügen eines neuen Fahrzeugs
+        //TODO select case oder if case zur abfrage des momentanen Fahrzeugs...
+        private void buttonAnlegen_Click(object sender, EventArgs e)
+        {
+            fahrzeugpool.neuenPKWAnlegen(textBoxHersteller.Text,textBoxModell.Text,textBoxKennzeichen.Text,Convert.ToInt32(textBoxZulassung.Text),float.Parse(textBoxAnschaffungspreis.Text),Convert.ToInt32(textBoxHubraum.Text),Convert.ToInt32(textBoxLeistung.Text),Convert.ToInt32(textBoxSchadstoffklasse.Text));
+        }
+
+        private void buttonAbbrechen_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(fahrzeugpool.Fahrzeugliste.Count());
+        }
+    }
+}
