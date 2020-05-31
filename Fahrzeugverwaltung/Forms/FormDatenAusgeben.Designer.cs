cb2d6f5fab6175c7def16e3088e1d12f1ff14e70
Anton Boger <Anton.Boger@edu.fhdw.de>
2020-05-31 20:44:02 +0200

    Test zur Datenausgabe

--- Fahrzeugverwaltung/Forms/FormDatenAusgeben.Designer.cs
+++ Fahrzeugverwaltung/Forms/FormDatenAusgeben.Designer.cs
@@ -31,6 +31,7 @@
             this.buttonAlleDaten = new System.Windows.Forms.Button();
             this.buttonFahrzeugDaten = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.listBoxDaten = new System.Windows.Forms.ListBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,6 +43,7 @@
             this.buttonAlleDaten.TabIndex = 0;
             this.buttonAlleDaten.Text = "Alle Daten ausgeben";
             this.buttonAlleDaten.UseVisualStyleBackColor = true;
+            this.buttonAlleDaten.Click += new System.EventHandler(this.buttonAlleDaten_Click);
             // 
             // buttonFahrzeugDaten
             // 
@@ -56,19 +58,31 @@
             // 
             this.panel1.Controls.Add(this.buttonAlleDaten);
             this.panel1.Controls.Add(this.buttonFahrzeugDaten);
-            this.panel1.Location = new System.Drawing.Point(255, 108);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(251, 202);
             this.panel1.TabIndex = 2;
+            // 
+            // listBoxDaten
+            // 
+            this.listBoxDaten.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.listBoxDaten.FormattingEnabled = true;
+            this.listBoxDaten.ItemHeight = 20;
+            this.listBoxDaten.Location = new System.Drawing.Point(12, 228);
+            this.listBoxDaten.Name = "listBoxDaten";
+            this.listBoxDaten.Size = new System.Drawing.Size(776, 224);
+            this.listBoxDaten.TabIndex = 3;
             // 
             // FormDatenAusgeben
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxDaten);
             this.Controls.Add(this.panel1);
             this.Name = "FormDatenAusgeben";
             this.Text = "FormDatenAusgeben";
+            this.Load += new System.EventHandler(this.FormDatenAusgeben_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -79,5 +93,6 @@
         private System.Windows.Forms.Button buttonAlleDaten;
         private System.Windows.Forms.Button buttonFahrzeugDaten;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBoxDaten;
     }
 }
--- Fahrzeugverwaltung/Forms/FormDatenAusgeben.cs
+++ Fahrzeugverwaltung/Forms/FormDatenAusgeben.cs
@@ -18,5 +18,21 @@ namespace Fahrzeugverwaltung.Forms
             InitializeComponent();
             fahrzeugpool = aFahrzeugpool;
         }
+
+        private void buttonAlleDaten_Click(object sender, EventArgs e)
+        {
+            string output = "{0,-15}\t{1,-15}\t{2,-15}\t{3,-15}\t{4,-15}\t{5,-15}\t";
+            for (int i = 0; i < 202; i++)
+            {
+                listBoxDaten.Items.Add(string.Format(output, "AUDI", "A/", "KSJH", "2009", "200", "PKW"));
+            }
+
+        }
+
+        private void FormDatenAusgeben_Load(object sender, EventArgs e)
+        {
+            string output = "{0,-15}\t{1,-15}\t{2,-15}\t{3,-15}\t{4,-15}\t{5,-15}\t";
+            listBoxDaten.Items.Add(string.Format(output, "Hersteller", "Modell", "Kennzeichen", "Erstzulassaung", "Anschaffungspreis", "Typ"));
+        }
     }
 }
